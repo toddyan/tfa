0a0771200664eaa6d887c456816e7c8bc53e7c47
toddyan <toddyan@tencent.com>
2018-07-24 23:05:31 +0800

    add image process

--- transfer/image_process.py
+++ transfer/image_process.py
@@ -2,59 +2,64 @@ import globalconf
 import os
 import tensorflow as tf
 import numpy as np
-import glob
 from tensorflow.python.platform import gfile
 
-def create_image_lists(s, input_dir, valid_rate, test_rate):
-    sub_dirs = [x[0] for x in os.walk(input_dir)][1:]
-    extensions = ['jpg','jepg','JPG','JPEG']
-
+def npy_maker(input_dir):
+    inputs = []
+    for d in os.listdir(input_dir):
+        if not os.path.isdir(os.path.join(input_dir, d)): continue
+        inputs.append([len(inputs), os.path.join(input_dir, d)])
+    # class by class process
+    for task in inputs:
+        print(task)
+        with tf.Session() as s:
+            part_npy_path = os.path.join(input_dir, str(task[0]) + ".npy")
+            arr = []
+            for img in os.listdir(task[1]):
+                if not img.endswith(".jpg"): continue
+                file = os.path.join(task[1], img)
+                image_data = gfile.FastGFile(file, 'rb').read()
+                image = tf.image.decode_jpeg(image_data)
+                image = tf.image.convert_image_dtype(image, dtype=tf.float32)
+                image = tf.image.resize_images(image, (299, 299))
+                value = s.run(image)
+                arr.append(value)
+            nparr = np.asarray(arr)
+            np.save(part_npy_path, nparr)
+            del arr
+            del nparr
+def combine(input_dir,num_classes,valid_rate,test_rate):
     training_images = []
     training_labels = []
     validation_images = []
     validation_labels = []
     testing_iamges = []
     testing_labels = []
-    label = 0
-    for dir in sub_dirs:
-        print(dir)
-        files = []
-        dir_name = os.path.basename(dir)
-        for ext in extensions:
-            file_glob = os.path.join(input_dir, dir_name, '*.' + ext)
-            files.extend(glob.glob(file_glob))
-        for file in files:
-            image_data = gfile.FastGFile(file, 'rb').read()
-            image = tf.image.decode_jpeg(image_data)
-            image = tf.image.convert_image_dtype(image, dtype=tf.float32)
-            image = tf.image.resize_images(image, (299,299))
-            value = s.run(image)
+    for label in range(num_classes):
+        part_npy_path = os.path.join(input_dir, str(label) + ".npy")
+        nparr = np.load(part_npy_path)
+        print(nparr.shape)
+        for img in nparr:
             rand = np.random.rand()
             if rand <= test_rate:
-                testing_iamges.append(value)
+                testing_iamges.append(img)
                 testing_labels.append(label)
             elif rand <= test_rate + valid_rate:
-                validation_images.append(value)
+                validation_images.append(img)
                 validation_labels.append(label)
             else:
-                training_images.append(value)
+                training_images.append(img)
                 training_labels.append(label)
-        label += 1
     random_state = np.random.get_state()
     np.random.shuffle(training_images)
     np.random.set_state(random_state)
     np.random.shuffle(training_labels)
-    return np.asarray([training_images,training_labels,validation_images,validation_labels,testing_iamges,testing_labels])
+    return np.asarray(training_images), np.asarray(training_labels),\
+           np.asarray(validation_images), np.asarray(validation_labels),\
+           np.asarray(testing_iamges), np.asarray(testing_labels)
 def main(argv=None):
-    # http://download.tensorflow.org/example_images/flower_photos.tgz
-    input_dir = globalconf.get_root() + 'transfer/flower_photos'
-    output_path = globalconf.get_root() + 'transfer/flower_photos/flower_photos.npy'
-    valid_rate = 0.1
-    test_rate = 0.1
-    with tf.Session() as s:
-        data = create_image_lists(s, input_dir, valid_rate, test_rate)
-        print(data.shape)
-        np.save(output_path, data)
-
+    img_train, label_train, img_valid, label_valid, img_test, label_test\
+        = combine(globalconf.get_root() + 'transfer/small', 5, 0.1,0.1)
+    print(img_train.shape, label_train.shape, img_valid.shape, label_valid.shape, img_test.shape, label_test.shape)
 if __name__ == "__main__":
     tf.app.run()
--- transfer/image_resizer.py
+++ transfer/image_resizer.py
@@ -1,3 +1,4 @@
+import globalconf
 import PIL.Image as Image
 import os
 
@@ -6,8 +7,14 @@ def image_resize(src, dst):
     img.save(dst)
 
 def image_dir_resize(src, dst):
-    if os.path.isfile(src):
+    if os.path.isfile(src) and src.endswith(".jpg"):
         Image.open(src).convert('RGB').resize((299, 299), Image.ANTIALIAS).save(dst)
+    if os.path.isdir(src):
+        if not os.path.exists(dst): os.mkdir(dst)
+        for f in os.listdir(src):
+            image_dir_resize(os.path.join(src, f),os.path.join(dst, f))
 
-image_resize("D:/tfroot/transfer/1.jpg","D:/tfroot/transfer/299_299/1.jpg")
-print(os.path.isdir("D:/tfroot/transfer"))
+if __name__ == "__main__":
+    src = globalconf.get_root() + "transfer/flower_photos"
+    dst = globalconf.get_root() + "transfer/small"
+    image_dir_resize(src, dst)
