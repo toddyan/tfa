3da2064b063651a0c2d64b83927e6ff650f9f6dd
toddyan <toddyan@tencent.com>
2018-07-26 00:51:41 +0800

    add image process & model restore

--- transfer/image_process.py
+++ transfer/image_process.py
@@ -4,62 +4,75 @@ import tensorflow as tf
 import numpy as np
 from tensorflow.python.platform import gfile
 
-def npy_maker(input_dir):
-    inputs = []
-    for d in os.listdir(input_dir):
-        if not os.path.isdir(os.path.join(input_dir, d)): continue
-        inputs.append([len(inputs), os.path.join(input_dir, d)])
-    # class by class process
-    for task in inputs:
-        print(task)
-        with tf.Session() as s:
-            part_npy_path = os.path.join(input_dir, str(task[0]) + ".npy")
-            arr = []
-            for img in os.listdir(task[1]):
-                if not img.endswith(".jpg"): continue
-                file = os.path.join(task[1], img)
-                image_data = gfile.FastGFile(file, 'rb').read()
-                image = tf.image.decode_jpeg(image_data)
-                image = tf.image.convert_image_dtype(image, dtype=tf.float32)
-                image = tf.image.resize_images(image, (299, 299))
-                value = s.run(image)
-                arr.append(value)
-            nparr = np.asarray(arr)
-            np.save(part_npy_path, nparr)
-            del arr
+class ImageDataset:
+    def npy_maker(self, input_dir):
+        inputs = []
+        for d in os.listdir(input_dir):
+            if not os.path.isdir(os.path.join(input_dir, d)): continue
+            inputs.append([len(inputs), os.path.join(input_dir, d)])
+        # class by class process
+        for task in inputs:
+            print(task)
+            with tf.Session() as s:
+                part_npy_path = os.path.join(input_dir, str(task[0]) + ".npy")
+                arr = []
+                for img in os.listdir(task[1]):
+                    if not img.endswith(".jpg"): continue
+                    file = os.path.join(task[1], img)
+                    image_data = gfile.FastGFile(file, 'rb').read()
+                    image = tf.image.decode_jpeg(image_data)
+                    image = tf.image.convert_image_dtype(image, dtype=tf.float32)
+                    image = tf.image.resize_images(image, (299, 299))
+                    value = s.run(image)
+                    arr.append(value)
+                nparr = np.asarray(arr)
+                np.save(part_npy_path, nparr)
+                del arr
+                del nparr
+    def combine(self, input_dir, num_classes, valid_rate, test_rate, sample_rate):
+        training_images = []
+        training_labels = []
+        validation_images = []
+        validation_labels = []
+        testing_iamges = []
+        testing_labels = []
+        for label in range(num_classes):
+            part_npy_path = os.path.join(input_dir, str(label) + ".npy")
+            nparr = np.load(part_npy_path)
+            print(nparr.shape)
+            for img in nparr:
+                if sample_rate is not None and np.random.rand() > sample_rate: continue
+                rand = np.random.rand()
+                if rand <= test_rate:
+                    testing_iamges.append(img)
+                    testing_labels.append(label)
+                elif rand <= test_rate + valid_rate:
+                    validation_images.append(img)
+                    validation_labels.append(label)
+                else:
+                    training_images.append(img)
+                    training_labels.append(label)
             del nparr
-def combine(input_dir,num_classes,valid_rate,test_rate):
-    training_images = []
-    training_labels = []
-    validation_images = []
-    validation_labels = []
-    testing_iamges = []
-    testing_labels = []
-    for label in range(num_classes):
-        part_npy_path = os.path.join(input_dir, str(label) + ".npy")
-        nparr = np.load(part_npy_path)
-        print(nparr.shape)
-        for img in nparr:
-            rand = np.random.rand()
-            if rand <= test_rate:
-                testing_iamges.append(img)
-                testing_labels.append(label)
-            elif rand <= test_rate + valid_rate:
-                validation_images.append(img)
-                validation_labels.append(label)
-            else:
-                training_images.append(img)
-                training_labels.append(label)
-    random_state = np.random.get_state()
-    np.random.shuffle(training_images)
-    np.random.set_state(random_state)
-    np.random.shuffle(training_labels)
-    return np.asarray(training_images), np.asarray(training_labels),\
-           np.asarray(validation_images), np.asarray(validation_labels),\
-           np.asarray(testing_iamges), np.asarray(testing_labels)
+        random_state = np.random.get_state()
+        np.random.shuffle(training_images)
+        np.random.set_state(random_state)
+        np.random.shuffle(training_labels)
+        self.training_images = np.asarray(training_images)
+        self.training_labels = np.asarray(training_labels)
+        self.validation_images = np.asarray(validation_images)
+        self.validation_labels = np.asarray(validation_labels)
+        self.test_images = np.asarray(testing_iamges)
+        self.test_labels = np.asarray(testing_labels)
+        del training_images
+        del training_labels
+        del validation_images
+        del validation_labels
+        del testing_iamges
+        del testing_labels
+
 def main(argv=None):
-    img_train, label_train, img_valid, label_valid, img_test, label_test\
-        = combine(globalconf.get_root() + 'transfer/small', 5, 0.1,0.1)
-    print(img_train.shape, label_train.shape, img_valid.shape, label_valid.shape, img_test.shape, label_test.shape)
+    d = ImageDataset()
+    d.combine(globalconf.get_root() + 'transfer/small', 5, 0.1, 0.1, 0.5)
+    print(d.training_images.shape, d.training_labels.shape, d.validation_images.shape, d.validation_labels.shape, d.test_images.shape, d.test_labels.shape)
 if __name__ == "__main__":
     tf.app.run()
--- transfer/inception_v3_demo.py
+++ transfer/inception_v3_demo.py
@@ -1,8 +1,19 @@
-# -*- coding: utf8 -*-
+# -*- coding: utf-8 -*-
 import globalconf
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
 import tensorflow.contrib.slim.python.slim.nets.inception_v3 as inception_v3
+from image_process import ImageDataset
+
+def get_restore_vars():
+    restore_vars = []
+    print("get_model_variables")
+    for var in slim.get_model_variables():
+        print("  ",var.op.name)
+        if var.op.name.startswith('InceptionV3/Logits')\
+                or var.op.name.startswith('InceptionV3/AuxLogits'): continue
+        restore_vars.append(var)
+    return restore_vars
 
 arg_scope = inception_v3.inception_v3_arg_scope()
 print("arg_scope:")
@@ -20,8 +31,37 @@ with tf.variable_scope("input"):
 
 logits, _ = inception_v3.inception_v3(images, num_classes=num_classes)
 
+with tf.variable_scope("optimize"):
+    tf.losses.sparse_softmax_cross_entropy(labels=labels,logits=logits)
+    train_op = tf.train.RMSPropOptimizer(0.00001).minimize(tf.losses.get_total_loss())
+
+with tf.variable_scope("eval"):
+    accuracy = tf.reduce_mean(tf.cast(tf.equal(
+        labels,
+        tf.argmax(logits, axis=1, output_type=tf.int32)
+    ),tf.float32))
+
+restore_fn = slim.assign_from_checkpoint_fn(ckpt_path,get_restore_vars(),ignore_missing_vars=True)
+
+# demonstrate
 print("trainable_variables:")
 for v in tf.trainable_variables():
     print("  ", v)
-#writer = tf.summary.FileWriter(logdir=logdir, graph=tf.get_default_graph())
-#writer.close()
+writer = tf.summary.FileWriter(logdir=logdir, graph=tf.get_default_graph())
+writer.close()
+
+with tf.Session() as s:
+    tf.global_variables_initializer().run()
+    restore_fn(s)
+    data = ImageDataset()
+    data.combine(globalconf.get_root() + 'transfer/small', 5, 0.1, 0.1, 0.5)
+    start = 0
+    batch_size = 32
+    for step in range(1000):
+        if start >= data.training_images.shape[0]: start = 0
+        end = min(start+batch_size, data.training_images.shape[0])
+        _, acc = s.run([train_op, accuracy], feed_dict={images:data.training_images[start:end], labels:data.training_labels[start:end]})
+        print(step,acc)
+        start = end
+
+
