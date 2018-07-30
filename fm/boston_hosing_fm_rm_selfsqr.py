56131cb40e45f587c2a7c65b82efdd25910c50d7
toddyan <yxdcsu@foxmail.com>
2018-07-30 20:55:17 +0800

    add boston housing/fm

--- fm/boston_hosing_fm_rm_selfsqr.py
+++ fm/boston_hosing_fm_rm_selfsqr.py
@@ -12,9 +12,9 @@ x_test = (x_test-mean)/std
 feature = tf.placeholder(dtype=tf.float32, shape=[None, 13], name="feature")
 price   = tf.placeholder(dtype=tf.float32, shape=[None],     name="price")
 u = tf.get_variable("weight", shape=[13,1], dtype=tf.float32, initializer=tf.truncated_normal_initializer(stddev=0.1))
-W = tf.get_variable("sqr_weight", shape=[13,6], dtype=tf.float32, initializer=tf.truncated_normal_initializer(stddev=0.1))
+W = tf.get_variable("sqr_weight", shape=[13,2], dtype=tf.float32, initializer=tf.truncated_normal_initializer(stddev=0.1))
 b = tf.get_variable("bias", shape=[], dtype=tf.float32, initializer=tf.constant_initializer())
-emb = tf.matmul(feature, W) # [None, 4]
+emb = tf.matmul(feature, W) # [None, k]
 sqr_part = tf.reshape(tf.diag_part(tf.matmul(emb, tf.transpose(emb))), shape=[-1,1])
 selfsqr_part = tf.reshape(tf.reduce_sum(W*W), shape=[-1,1])
 pred = b + tf.matmul(feature, u) + sqr_part - selfsqr_part
@@ -33,8 +33,8 @@ with tf.Session() as sess:
         if step % 1000 == 0:
             print(step, sess.run(loss, feed_dict={feature:x_test, price:y_test}))
 
-# fm: learning_rate 0.0001, batch_size 32, emb 6, test_loss 84.35
-# fm: learning_rate 0.0001, batch_size 32, emb 5, test_loss
-# fm: learning_rate 0.0001, batch_size 32, emb 4, test_loss
-# fm: learning_rate 0.0001, batch_size 32, emb 3, test_loss
-# fm: learning_rate 0.0001, batch_size 32, emb 2, test_loss
+# fm: learning_rate 0.0001, batch_size 32, emb 6, test_loss 84.20
+# fm: learning_rate 0.0001, batch_size 32, emb 5, test_loss 84.20
+# fm: learning_rate 0.0001, batch_size 32, emb 4, test_loss 84.20
+# fm: learning_rate 0.0001, batch_size 32, emb 3, test_loss 84.11
+# fm: learning_rate 0.0001, batch_size 32, emb 2, test_loss 84.05
