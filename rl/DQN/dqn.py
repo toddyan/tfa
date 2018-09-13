63d63f1be5596f78b47e576d4b0de020bdcf9d56
toddyan <yxdcsu@foxmail.com>
2018-09-13 09:10:38 +0800

    dqn

--- rl/DQN/dqn.py
+++ rl/DQN/dqn.py
@@ -8,47 +8,66 @@ class Agent:
             with tf.variable_scope(net_name):
                 input = tf.placeholder(dtype=tf.float32, shape=[None, n_features], name="observation")
                 w_initializer = tf.random_normal_initializer(mean=0.0, stddev=0.1)
-                b_initializer = tf.constant_initializer(0.0)
-                n_hidden = [64,32]
+                b_initializer = tf.constant_initializer(0.1)
+                n_hidden = [80,40]
                 with tf.variable_scope("l1"):
                     w = tf.get_variable("w", shape=[n_features, n_hidden[0]], initializer=w_initializer,
                                         collections=collection_name)
                     b = tf.get_variable("b", shape=[n_hidden[0]], initializer=b_initializer, collections=collection_name)
-                    l1 = tf.nn.relu(tf.matmul(input, w) + b)
-                with tf.variable_scope("l2"):
-                    w = tf.get_variable("w", shape=[n_hidden[0], n_hidden[1]], initializer=w_initializer,
-                                        collections=collection_name)
-                    b = tf.get_variable("b", shape=[n_hidden[1]], initializer=b_initializer, collections=collection_name)
-                    l2 = tf.nn.relu(tf.matmul(l1, w) + b)
+                    a = tf.nn.relu(tf.matmul(input, w) + b)
+                # with tf.variable_scope("l2"):
+                #     w = tf.get_variable("w", shape=[n_hidden[0], n_hidden[1]], initializer=w_initializer,
+                #                         collections=collection_name)
+                #     b = tf.get_variable("b", shape=[n_hidden[1]], initializer=b_initializer, collections=collection_name)
+                #     a = tf.nn.relu(tf.matmul(a, w) + b)
                 if dualing:
                     with tf.variable_scope("l3"):
                         with tf.variable_scope("value"):
-                            w = tf.get_variable("w", shape=[n_hidden[1], 1], initializer=w_initializer,
+                            w = tf.get_variable("w", shape=[n_hidden[0], 1], initializer=w_initializer,
                                                 collections=collection_name)
                             b = tf.get_variable("b", shape=[1], initializer=b_initializer, collections=collection_name)
-                            value = tf.matmul(l2, w) + b
+                            value = tf.matmul(a, w) + b
                         with tf.variable_scope("advantage"):
-                            w = tf.get_variable("w", shape=[n_hidden[1], n_actions], initializer=w_initializer,
+                            w = tf.get_variable("w", shape=[n_hidden[0], n_actions], initializer=w_initializer,
                                                 collections=collection_name)
                             b = tf.get_variable("b", shape=[n_actions], initializer=b_initializer, collections=collection_name)
-                            advantage = tf.matmul(l2, w) + b
-                        l3 = value + (advantage - tf.reduce_mean(advantage, axis=1, keepdims=True))
+                            advantage = tf.matmul(a, w) + b
+                        a = value + (advantage - tf.reduce_mean(advantage, axis=1, keepdims=True))
                 else:
                     with tf.variable_scope("l3"):
                         with tf.variable_scope("value"):
-                            w = tf.get_variable("w", shape=[n_hidden[1], n_actions], initializer=w_initializer,
+                            w = tf.get_variable("w", shape=[n_hidden[0], n_actions], initializer=w_initializer,
                                                 collections=collection_name)
                             b = tf.get_variable("b", shape=[n_actions], initializer=b_initializer, collections=collection_name)
-                            l3 = tf.matmul(l2, w) + b
-                return input, l3
+                            a = tf.matmul(a, w) + b
+                return input, a
+        def _conv_DQN(feature_shape, n_actions, net_name, collection_name, dualing):
+            with tf.variable_scope(net_name):
+                input = tf.placeholder(dtype=tf.float32, shape=[None]+feature_shape, name="observation")
+                w_initializer = tf.random_normal_initializer(mean=0.0, stddev=0.1)
+                b_initializer = tf.constant_initializer(0.0)
+                n_kernels = [8, 8]
+                with tf.variable_scope("l1"):
+                    w = tf.get_variable("w", shape=[3, 3, 1, n_kernels[0]], initializer=w_initializer,
+                                        collections=collection_name)
+                    b = tf.get_variable("b", shape=[n_kernels[0]], initializer=b_initializer, collections=collection_name)
+                    a = tf.nn.conv2d(input, w, strides=[1,1,1,1], padding='SAME')
+                    a = tf.nn.relu(tf.nn.bias_add(a, b))
+                    a = tf.nn.max_pool(a, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+                with tf.variable_scope("l2"):
+                    w = tf.get_variable("w", shape=[3, 3, n_kernels[0], n_kernels[1]], initializer=w_initializer,
+                                        collections=collection_name)
+                    b = tf.get_variable("b", shape=[feature_shape[1]], initializer=b_initializer, collections=collection_name)
+                    a = tf.nn.conv2d(a, w, strides=[1,1,1,1], padding='SAME')
+                    a = tf.nn.relu(tf.nn.bias_add(a, b))
         def _net_builder(n_features, n_actions, learning_rate, dueling):
             net_a_input, net_a_output = _DQN(n_features, n_actions, 'net_a', ['net_a', tf.GraphKeys.GLOBAL_VARIABLES], dueling)
             net_b_input, net_b_output = _DQN(n_features, n_actions, 'net_b', ['net_b', tf.GraphKeys.GLOBAL_VARIABLES], dueling)
             with tf.variable_scope("loss"):
                 target = tf.placeholder(tf.float32, shape=[None, n_actions], name="target")
                 loss = tf.reduce_mean(tf.squared_difference(net_a_output, target))
-                # train_op = tf.train.RMSPropOptimizer(learning_rate).minimize(loss)
-                train_op = tf.train.AdamOptimizer(learning_rate).minimize(loss)
+                train_op = tf.train.RMSPropOptimizer(learning_rate).minimize(loss)
+                #train_op = tf.train.AdamOptimizer(learning_rate).minimize(loss)
             with tf.variable_scope("net_assign"):
                 net_assign_op = [tf.assign(a, b) for a, b in zip(tf.get_collection('net_a'), tf.get_collection('net_b'))]
             return net_a_input, net_a_output, net_b_input, net_b_output, target, loss, train_op, net_assign_op
@@ -65,7 +84,7 @@ class Agent:
         self.n_feature = n_feature
         self.n_action = n_action
         self.reward_decay = 0.9
-        self.net_assign_step = 1000
+        self.net_assign_step = 2000
         self.batch_size = 16
         self.explore_epsilon = 1.0
         self.epsilon_decay = 0.999
@@ -106,12 +125,15 @@ class Agent:
     def learn(self, verbose):
         if self.learn_counter % self.net_assign_step == 0:
             self.sess.run(self.net_assign_op)
+            print(" *")
         batch_index = np.random.choice(min(self.memory_counter, self.memory_capacity), self.batch_size)
-        if verbose:
-            print("batch_index:", batch_index)
+
         batch_observation = self.memories['observation'][batch_index]
         batch_observation_ = self.memories['observation_'][batch_index]
-
+        if verbose:
+            print("batch_index:", batch_index)
+            print("observation:", batch_observation[:2,:40])
+            print("q:", self.sess.run(self.net_a_output, feed_dict={self.net_a_input: batch_observation})[:2])
         batch_action = self.memories['action'][batch_index]
         batch_reward = self.memories['reward'][batch_index]
         predict_q, next_q = self.sess.run([self.net_a_output, self.net_b_output], feed_dict={
@@ -127,8 +149,8 @@ class Agent:
             self.net_a_input: batch_observation, self.target: target
         })
         if verbose:
-            print("observation:", batch_observation.std(axis=0))
-            print("q:", self.sess.run(self.net_a_output, feed_dict={self.net_a_input: batch_observation}))
+            print("after update")
+            print("q:", self.sess.run(self.net_a_output, feed_dict={self.net_a_input: batch_observation})[:2])
         '''
         q:
         [ 1  2 _3_]
@@ -144,26 +166,46 @@ class Agent:
         self.learn_counter += 1
 
 np.random.seed(0)
-agent = Agent(128, 6, 0.1, True)
+agent = Agent(128, 2, 0.01, True)
 env = gym.make('Pong-ramNoFrameskip-v0')
+# env = gym.make('PongNoFrameskip-v4')
+
+
 for episode in range(100000):
     observation = env.reset()
+    o2 = observation
     total_reward = 0.0
     for step in range(10000):
-        if episode % 100 == 0:
+        if episode % 200 == 0:
             env.render()
         action = agent.choose_action(observation)
-        observation_, reward, done, info = env.step(action)
-        agent.add_memory(observation/255.0, action, observation_/255.0, reward)
+        observation_, reward, done, info = env.step(action+4) # 4 up, 5 down
+        agent.add_memory(observation/1.0-o2/1.0, action, observation_/1.0-observation/1.0, reward)
+        o2 = observation
         observation = observation_
         if reward > 0:
             print(episode, step, reward)
         total_reward += reward
         if (step > 200 or episode > 0) and step % 5 == 0 :
-            agent.learn(verbose=False)
+            verbose = (episode % 200 ==0 and step == 1000)
+            agent.learn(verbose)
         if done:
             print("episode %d end with step %d and total reward %f" % (episode, step, total_reward))
             break
     print("explore:", agent.explore_decay())
 
-
+'''
+import matplotlib.pyplot as plt
+import PIL.Image as Image
+    for st in range(10000):
+        observation,_,_,_ = env.step(1)
+        if st % 10 == 0:
+            img = Image.fromarray(observation.astype("uint8")).convert('L').resize((32, 42), Image.AFFINE)
+            plt.imshow(img)
+            plt.show()
+    print(observation.shape)
+    img = Image.fromarray(observation.astype("uint8")).convert('L').resize((32,42), Image.AFFINE)
+    plt.imshow(img)
+    plt.show()
+    exit()
+'''
